da01171d2121546b5393ee9d5c7e4b84c286246a
Matthias Weber <34288998+KallDozer@users.noreply.github.com>
2018-01-18 17:06:06 -0500

    Update road_sign.py

--- code/road_sign.py
+++ code/road_sign.py
@@ -3,6 +3,7 @@ from control_info_element import control_info_element
 class road_sign(control_info_element):
     ''' A general road_sign class '''
     def __init__ (self, i, x, y, r, v):
+        '''Intializes the road sign'''
         self.locationX = x
         self.locationY = y
         self.belongstoRoad = r
@@ -10,4 +11,5 @@ class road_sign(control_info_element):
         control_info_element.__init__(self, i)
 
     def changeValue (self):
+        '''This function is responsible for changing the shown value on the digital road sign'''
         return None
