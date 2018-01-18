92f6264149a1a7e908f0ac94329a0ddb0fe5ff65
Matthias Weber <34288998+KallDozer@users.noreply.github.com>
2018-01-18 17:19:27 -0500

    Update control_info_element.py

--- code/control_info_element.py
+++ code/control_info_element.py
@@ -1,7 +1,10 @@
 class control_info_element:
     ''' A general control_info_element class '''
+    
     def __init__ (self, i):
+        '''Intializes the general control and info element'''
         self.ID = i
 
     def pushInfo (self):
+        '''This function is responsible for pushing specific information to the TCU'''
         return None
